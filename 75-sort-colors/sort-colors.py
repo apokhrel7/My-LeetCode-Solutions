7cbe7d65004be66d9346e4d1a18566f1daae7faf
Anish Pokhrel <66049909+apokhrel7@users.noreply.github.com>
2024-12-01 23:06:32 -0700

    Time: 0 ms (100.00%) | Memory: 17.3 MB (5.50%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -4,13 +4,33 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
 
-        # use insertion sort
+        # Use insertion sort
 
-        p0, p1 = 0, 1
-        n = len(nums)
+        # p0, p1 = 0, 1
+        # n = len(nums)
 
-        for i in range(n - 1):
-            for j in range(i+1, n):
-                if nums[i] > nums[j]:
-                    nums[i], nums[j] = nums[j], nums[i]
+        # for i in range(n - 1):
+        #     for j in range(i+1, n):
+        #         if nums[i] > nums[j]:
+        #             nums[i], nums[j] = nums[j], nums[i]
+
+
+        ### Use hashmap and bucket sort ###
+        dict = {}
+
+        for num in nums:
+            if num in dict:
+                dict[num] += 1
+            else:
+                dict[num] = 1
+
+        # dict = {2: 2, 0: 2, 1: 2}
+        start = 0
+        for i in range(3):
+            if i in dict:
+                nums[start : start + dict[i]] = [i] * dict[i]
+                start += dict[i]
+
         
+
+         
