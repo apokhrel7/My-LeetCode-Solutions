34469aab1e08523837862cf52a051a3e095d21c3
Anish Pokhrel <66049909+apokhrel7@users.noreply.github.com>
2025-01-30 23:36:17 -0700

    Time: 183 ms (16.60%) | Memory: 78.7 MB (14.06%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,69 +1,68 @@
-# Create doubly linked list data structure
-# This data structure will hold key, value pair
-
+# Create node class
 class Node:
     def __init__(self, key, val):
-        self.key = key
-        self.val = val
-
-        self.prev = None  # leftmost dummy node holding Least Recently used (LRU) cache
-        self.next = None  # rightmost dummy node holding Most Recently Used (MRU) cache
-
+        self.key, self.val = key, val
+        self.prev = self.next = None
 
 class LRUCache:
     def __init__(self, capacity: int):
-        self.cache = {}  # maps key to node
-        self.capacity = capacity
+        self.cap = capacity
+        self.hashmap = {}       # map the key to node
 
-        # left = LRU, right = most recently used 
+        # left = LRU, right = MRU (most recently used)
         self.left, self.right = Node(0, 0), Node(0, 0)
-        self.left.next, self.right.prev = self.right, self.left
+        self.left.next, self.right.prev = self.right, self.left  # attach the LRU and MRU (most recently used) to each other in beginning
 
-    def insert(self, node):
-        # must be inserted on the right as its recently used
-
-        # prev, nxt = self.right.prev, self.right
-        # prev.next = nxt.prev = node
-        # node.next, node.prev = nxt, prev
-
-        prev = self.right.prev  # node on left of rightmost dummy node
-        prev.next, node.prev = node, prev  # attach node on left to newly inserted node, attach pointer from new node to that prev node
-        self.right.prev, node.next = node, self.right   # attach new node to rightmost dummy node
-
-
+    # remove node from list
     def remove(self, node):
-        prev, nxt = node.prev, node.next
-        prev.next, nxt.prev = nxt, prev
-    
+        # nextNode = node.next  # node that is going to be at leftmost position now
+        node.prev.next, node.next.prev = node.next, node.prev
+
+        # # attach new node next to LRU node
+        # self.left.next = nextNode
+        # nextNode.prev = self.left
+
+
+
+
+
+    # insert node at right
+    def insert(self, node):
+        prevNode = self.right.prev
+        self.right.prev, node.next = node, self.right
+        node.prev, prevNode.next = prevNode, node 
+        
 
     def get(self, key: int) -> int:
-        if key in self.cache:
-            # remove the key from its original position and insert it into beginning of cache as it was just accessed
-            self.remove(self.cache[key])
-            self.insert(self.cache[key])
+        if key in self.hashmap:
+            # TODO: update most recently used
 
-            return self.cache[key].val
+            # we need to remove the key-value pair from left (LRU) and reinsert it back to the right (MRU)
+            # this way we can capture LRU and MRU nodes
+            self.remove(self.hashmap[key])  
+            self.insert(self.hashmap[key])
+
+            return self.hashmap[key].val   # hashmap[key] = pointer of node, so use .val to determine the actual node value
 
         return -1
-        
 
     def put(self, key: int, value: int) -> None:
 
-        # if key already in cache
-        # since we are inserting same key again, this key is most recently accessed so it needs to be removed and later inserted to most recently accessed position
-        if key in self.cache:
-            self.remove(self.cache[key])
+        # remove node if already exists
+        if key in self.hashmap:
+            self.remove(self.hashmap[key]) 
 
-        # insert the key, node pair into cache
-        self.cache[key] = Node(key, value)
-        self.insert(self.cache[key])
+        # insert new node to list
+        newNode = Node(key, value)
+        self.hashmap[key] = newNode
+        self.insert(self.hashmap[key])  
 
-        # remove the node from linked list and cache if capacity has been reached
-        if len(self.cache) > self.capacity:
-            lru = self.left.next    # LRU will be found on left side of list (right side of dummy node)
-            self.remove(lru)        # Remove the node from the list
-            del self.cache[lru.key] # Remove the key, pointer pair from cache
-        
+        if len(self.hashmap) > self.cap:
+            # remove from linkedin list and delete the LRU from hashmap
+
+            lru = self.left.next
+            self.remove(lru)        # remove from linked list
+            del self.hashmap[lru.key]
 
 
 # Your LRUCache object will be instantiated and called as such:
