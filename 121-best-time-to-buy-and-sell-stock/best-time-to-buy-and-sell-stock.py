08ef4f7ab496a76ec4a89c7d7242acdb7442e81e
Anish Pokhrel <66049909+apokhrel7@users.noreply.github.com>
2025-02-04 00:54:20 -0700

    Time: 43 ms (78.67%) | Memory: 27.1 MB (8.16%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,25 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+
+        max_profit = 0
+
+        buy = prices[0]
+        for i in range(1, len(prices)):
+            # if see a low price, then consider buying it unless there's a lower price
+            # if see high price, check the difference see if its high then do transaction
+
+            if prices[i] < buy:
+                buy = prices[i]
+
+            profit = prices[i] - buy
+
+
+            if profit > max_profit:
+                max_profit = max(max_profit, profit)
+
+        return max_profit
+
+
+            
+
+        
