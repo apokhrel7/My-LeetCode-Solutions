e2dbc30554eba01e849fd6e9dca1fe21a3817455
Anish Pokhrel <66049909+apokhrel7@users.noreply.github.com>
2025-02-04 00:32:24 -0700

    Time: 180 ms (93.79%) | Memory: 28.1 MB (94.19%) - LeetSync

---
+++ 3594-identify-the-largest-outlier-in-an-array/identify-the-largest-outlier-in-an-array.py
@@ -0,0 +1,37 @@
+class Solution:
+    def getLargestOutlier(self, nums: List[int]) -> int:
+        # n - 2 elements are special numbers
+        # 1 out of 2 elements is sum of these special numbers, other is outlier
+
+
+        # place elements in dictionary to quickly find it
+        dict = {}
+
+        for i in range(len(nums)):
+            if nums[i] not in dict:
+                dict[nums[i]] = 1
+            else:
+                dict[nums[i]] += 1
+
+        # total_sum = 2(sum_special_number) + outlier
+        # outlier = total_sum - 2(sum_special_nums)
+
+        total_sum = sum(nums)
+        max_outlier = float('-inf')
+
+        for key in dict:
+            # try each number and see if it is the sum special number
+            outlier = total_sum - 2*key
+
+            if outlier in dict.keys():
+                if key != outlier or dict[key] > 1:
+                    max_outlier = max(max_outlier, outlier)
+            
+
+        return max_outlier
+
+
+
+
+        
+        
