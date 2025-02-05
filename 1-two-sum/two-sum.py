e2753fa500658cd87b019167b162fe88dca9a99e
Anish Pokhrel <66049909+apokhrel7@users.noreply.github.com>
2025-02-05 01:21:25 -0700

    Time: 1 ms (68.09%) | Memory: 13.3 MB (43.47%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,44 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        # create hash table
+
+        # hashed_nums = {}
+            
+
+
+        # difference = 0
+        # for i in range(len(nums)):
+
+
+        #     difference = target - nums[i]
+
+        #     if difference in hashed_nums and hashed_nums[difference] != i :
+        #         return [i, hashed_nums[difference]]
+        #     hashed_nums[nums[i]] = i
+
+
+        # return []
+
+        dict = {}
+
+        for i in range(len(nums)):
+            complement = target - nums[i]
+
+            if complement in dict:
+                return [i, dict[complement]]
+
+            else:
+                dict[nums[i]] = i
+
+        return []
+
+
+                    
+                    
+        
+        
