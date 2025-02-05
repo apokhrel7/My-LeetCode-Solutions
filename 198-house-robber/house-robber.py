38cc5a8379f860dd5281832a1b45e19cd3a7500a
Anish Pokhrel <66049909+apokhrel7@users.noreply.github.com>
2025-02-05 02:21:40 -0700

    Time: 0 ms (100.00%) | Memory: 17.6 MB (81.61%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -2,13 +2,36 @@ class Solution:
     def rob(self, nums: List[int]) -> int:
         # recurrence relation
         # dp[i] = max(dp[i-1], dp[i-2] + nums[i])
+        # if len(nums) == 1: return nums[0]
+        # dp = [0] * len(nums)
+        # # Base cases
+        # dp[0] = nums[0]
+        # dp[1] = max(dp[0], nums[1])
+
+        # for i in range(2, len(nums)):
+        #     dp[i] = max(dp[i-1], dp[i-2] + nums[i])
+
+        # return dp[-1]
+
+        # we don't need to store whole array, just need previous values
+
         if len(nums) == 1: return nums[0]
         dp = [0] * len(nums)
         # Base cases
-        dp[0] = nums[0]
-        dp[1] = max(dp[0], nums[1])
+        max_robber = 0
+        prev_max_robber = 0
+     
 
-        for i in range(2, len(nums)):
-            dp[i] = max(dp[i-1], dp[i-2] + nums[i])
+        for i in range(len(nums)):
+            # dp[i] = max(dp[i-1], dp[i-2] + nums[i])
+            
+            temp = max(prev_max_robber + nums[i], max_robber)
+            prev_max_robber = max_robber
+            max_robber = temp
+            
 
-        return dp[-1]
+
+        return max_robber
+
+
+        
