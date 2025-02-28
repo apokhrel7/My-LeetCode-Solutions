7b190d96a3949ed2458c2c07f491f97d0761ff25
Anish Pokhrel <66049909+apokhrel7@users.noreply.github.com>
2025-02-27 23:15:15 -0700

    Time: 7 ms (49.40%) | Memory: 20 MB (10.34%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,42 +1,42 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        if numCourses == 1: return True
-        
-        
-        adjList = {}
+        adj_list = {}
 
-        {0: [], 1:[0]}
-    
-
-        # prepare adjacency list
+        # initialize adjacency list
         for i in range(numCourses):
-            adjList[i] = []
+            adj_list[i] = []
 
-        # add prerequisites to adjacency list
+        # populate adjacency list
         for crs, pre in prerequisites:
-            adjList[crs].append(pre)
+            adj_list[crs].append(pre)
 
-        seen = set() # to check for cycles
+        potential_cycles = set()  # keep track of potential courses that could be cycles
+        seen = set()              # keep track of courses we have already checked and verified to not be cycles
+
 
         def dfs(course):
-            if course in seen:
+            if course in potential_cycles:
                 return False
 
-            seen.add(course)
+            if course in seen:
+                return True
 
-            for pre in adjList[course]:
+            potential_cycles.add(course)        # this course could be a cycle, so add it to list and check
+
+            # check cycles by going through all the prerequisites of a course
+            for pre in adj_list[course]:
                 if not dfs(pre):
                     return False
 
-            seen.remove(course)
-            adjList[course] = []
+            potential_cycles.remove(course)     # at this point we have verified that this course doesn't have cycles (at the moment)
+            seen.add(course)                    # we have seen this course, so remember it (reduced time complexity?)
+
             return True
 
-        for i in range(numCourses):
-            if not dfs(i):
+
+        # need to check every course
+        for course in range(numCourses):
+            if not dfs(course):
                 return False
 
         return True
-        
-
-        
