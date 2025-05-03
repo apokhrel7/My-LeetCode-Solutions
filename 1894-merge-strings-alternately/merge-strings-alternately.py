7dbb214b40703128597219e98f2634b5757b40b9
Anish Pokhrel <66049909+apokhrel7@users.noreply.github.com>
2025-05-03 13:47:39 -0600

    Time: 41 ms (37.34%) | Memory: 17.9 MB (33.91%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.py
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -1,23 +1,24 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
+        ptr = 0
+        merged = ''
+
+        while ptr < len(word1) and ptr < len(word2):
+            merged += word1[ptr]
+            merged += word2[ptr]
+
+            ptr += 1
+
+        if ptr < len(word1):
+            merged += word1[ptr:]
+
+        elif ptr < len(word2):
+            merged += word2[ptr:]
+
+        return merged
+
         
-        res = ""
-
-
-        # loop using length of the longest word
-        longest_word = max(len(word1), len(word2))
-
-
-        for i in range(longest_word):
-            if i < len(word1):
-                res += word1[i]
-
-            if i < len(word2):
-                res += word2[i]
-
-        return res
 
 
 
-
-
+        
