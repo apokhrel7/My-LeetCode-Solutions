344c241f8b6236052a78201f97adb26f40b5d96f
Anish Pokhrel <66049909+apokhrel7@users.noreply.github.com>
2025-05-04 19:18:05 -0600

    Time: 51 ms (40.49%) | Memory: 19.8 MB (56.33%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.py
+++ 141-linked-list-cycle/linked-list-cycle.py
@@ -4,29 +4,23 @@
 #         self.val = x
 #         self.next = None
 
-
-### ALGORITHM: Fast and slow pointer ### 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
 
+        # use fast and slow pointer
+        # slow pointer will move up one increment at a time
+        # fast pointer will move double the amount
+        # If there is a cycle, then fast pointer will meet with slow pointer
+        # this is because if slow pointer is moving at increment s, then fast pointer is moving at 2s. in a cycle, 2s is same position as s
 
-        fast, slow = head, head
+        fast = slow = head
 
         while fast and fast.next:
-        
             slow = slow.next
             fast = fast.next.next
 
-            if fast == slow: return True
-
+            if fast == slow:
+                return True
 
         return False
-       
-
-        
-            
-
-
-
-        
         
