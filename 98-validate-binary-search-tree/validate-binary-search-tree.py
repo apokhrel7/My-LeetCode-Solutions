27150d522929b883e05ee6d7d1552a814c32550e
Anish Pokhrel <66049909+apokhrel7@users.noreply.github.com>
2025-05-06 00:59:57 -0600

    Time: 0 ms (100.00%) | Memory: 20 MB (8.50%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/validate-binary-search-tree.py
@@ -7,29 +7,23 @@
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
 
-        # recursive (dfs) solution using left and right node value boundries
-
+        # -inf < node < +inf
         def dfs(node, left_boundary, right_boundary):
-
-            # if leaf node is reached, return True as we haven't broken definition of BST
+            # Leaf node is considered a valid BST
             if not node:
                 return True
 
-            # not a BST, return False
+            # if the left subtree is not less than node's key or right_substree is not greater than node's key: return false
             if not (left_boundary < node.val < right_boundary):
                 return False
 
-            ### Recurse on left and right subtrees ###
-
-            # left subtree can only have values less than its parent (update right_boundary = val of parent node) 
-            # right subtree can only have values greater than its parent (update left_boundary = val of parent node) 
-
+            # go left subtree and right subtree
+            # when traversing left subtree, boundary becomes -inf < node.left.val < node.val
+            # when traversing right subtree, boundary becomes node.val < node.right.val < +inf
             return dfs(node.left, left_boundary, node.val) and dfs(node.right, node.val, right_boundary)
 
+        
 
-        # initialy root is bounded by -inf < root.val < inf (root can be any value)
-        return dfs(root, float('-inf'), float('inf'))  
+        return dfs(root, float("-inf"), float("inf"))
 
-
-
-            
+        
