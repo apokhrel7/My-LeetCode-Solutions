554f0aa81430e7d79906ea1e00c12dd8ec4e1fd7
Anish Pokhrel <66049909+apokhrel7@users.noreply.github.com>
2025-05-08 15:04:57 -0600

    Time: 0 ms (100.00%) | Memory: 17.9 MB (77.67%) - LeetSync

--- 404-sum-of-left-leaves/sum-of-left-leaves.py
+++ 404-sum-of-left-leaves/sum-of-left-leaves.py
@@ -1,5 +1,3 @@
-from collections import deque
-
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -8,42 +6,19 @@ from collections import deque
 #         self.right = right
 class Solution:
     def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
+        def dfs(node):
 
-        # def dfs(node):
-        #     if not node:
-        #         return 0
-        #     # if its a left leaf node
-        #     # if the left node exists but that left node doesnt have left and right children, its a leaf node
-        #     # hwoever, we still need to explore the right child of that node as well
-        #     if node.left and not node.left.left and not node.left.right:
-        #         return node.left.val + dfs(node.right)
+            # leaf node is always 0
+            if not node:
+                return 0
+
+            # if node is a leaf node (found if it doesnt have any left or right children)
+            # then add the value of that node + the left leaf of its right side (recurse on the right subtree of the parent)
+            if node.left and not node.left.left and not node.left.right:
+                return node.left.val + dfs(node.right)
+
+            # return the result of left and right subtree up to the parent
+            # this also covers the case where a leaf node is not found yet, so recurse on left and right subtrees
+            return dfs(node.left) + dfs(node.right)
             
-        #     # if we couldn't find leaf node, recurse on the left and right subtrees
-        #     return dfs(node.left) + dfs(node.right) 
-
-           
-
-        # return dfs(root)
-
-
-        q = deque([root])
-        res = 0
-      
-        while q:
-            q_length = len(q)
-
-           
-            node = q.popleft()
-
-            if node.left:
-                if not node.left.left and not node.left.right:
-                    res += node.left.val
-                q.append(node.left)
-            if node.right:
-                q.append(node.right)
-
-        return res
-
-        
-
-        
+        return dfs(root)
