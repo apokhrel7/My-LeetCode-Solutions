6873a32555bcbf09faf259793437957022cb7c54
Anish Pokhrel <66049909+apokhrel7@users.noreply.github.com>
2025-05-08 11:52:31 -0600

    Time: 0 ms (100.00%) | Memory: 18 MB (47.24%) - LeetSync

---
+++ 404-sum-of-left-leaves/sum-of-left-leaves.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
+
+        def dfs(node):
+            if not node:
+                return 0
+            # if its a left leaf node
+            # if the left node exists but that left node doesnt have left and right children, its a leaf node
+            # hwoever, we still need to explore the right child of that node as well
+            if node.left and not node.left.left and not node.left.right:
+                return node.left.val + dfs(node.right)
+            
+            # if we couldn't find leaf node, recurse on the left and right subtrees
+            return dfs(node.left) + dfs(node.right) 
+
+           
+
+        return dfs(root)
+
+        
