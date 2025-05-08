05aee34d42eef582db66bff6fd8986f2a044a316
Anish Pokhrel <66049909+apokhrel7@users.noreply.github.com>
2025-05-08 12:05:07 -0600

    Time: 0 ms (100.00%) | Memory: 18.2 MB (29.60%) - LeetSync

--- 404-sum-of-left-leaves/sum-of-left-leaves.py
+++ 404-sum-of-left-leaves/sum-of-left-leaves.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -7,20 +9,41 @@
 class Solution:
     def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
 
-        def dfs(node):
-            if not node:
-                return 0
-            # if its a left leaf node
-            # if the left node exists but that left node doesnt have left and right children, its a leaf node
-            # hwoever, we still need to explore the right child of that node as well
-            if node.left and not node.left.left and not node.left.right:
-                return node.left.val + dfs(node.right)
+        # def dfs(node):
+        #     if not node:
+        #         return 0
+        #     # if its a left leaf node
+        #     # if the left node exists but that left node doesnt have left and right children, its a leaf node
+        #     # hwoever, we still need to explore the right child of that node as well
+        #     if node.left and not node.left.left and not node.left.right:
+        #         return node.left.val + dfs(node.right)
             
-            # if we couldn't find leaf node, recurse on the left and right subtrees
-            return dfs(node.left) + dfs(node.right) 
+        #     # if we couldn't find leaf node, recurse on the left and right subtrees
+        #     return dfs(node.left) + dfs(node.right) 
 
            
 
-        return dfs(root)
+        # return dfs(root)
+
+
+        q = deque([root])
+        res = 0
+      
+        while q:
+            q_length = len(q)
+
+           
+            node = q.popleft()
+
+            if node.left:
+                if not node.left.left and not node.left.right:
+                    res += node.left.val
+                q.append(node.left)
+            if node.right:
+                q.append(node.right)
+
+        return res
+
+        
 
         
