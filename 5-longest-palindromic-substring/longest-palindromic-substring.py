c2fc04f5fb2641fa678387cc18535ff100a352dd
Anish Pokhrel <66049909+apokhrel7@users.noreply.github.com>
2025-05-08 20:09:26 -0600

    Time: 367 ms (45.55%) | Memory: 17.8 MB (84.15%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,72 +1,29 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
+
+        # can be even or odd
         if len(s) == 1: return s
-        
-        max_length_substring = ''
 
-        # def getPalindrome(left, right, substring) -> str:
-        #     while left >= 0 and right <= (len(s) - 1) and s[left] == s[right]:
-        #         left -= 1
-        #         right += 1
-
-        #     return s[left + 1: right]
-
+        max_length = ''
         for i in range(len(s)):
-            ### odd string (start from middle and expand out)
+            
+            # even
             left, right = i, i
-
-            while left >= 0 and right <= (len(s) - 1) and s[left] == s[right]:
-
-                if (right - left + 1) > len(max_length_substring):
-                    max_length_substring = s[left:right+1]
-
+        
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                if (right - left) + 1 > len(max_length):
+                    max_length = s[left:right+1]
                 left -= 1
-                right += 1
+                right += 1  
 
-            ### even string (start with character that is next to the current pointer)
+
+            #odd
             left, right = i, i + 1
-            while left >= 0 and right <= (len(s) - 1) and s[left] == s[right]:
-
-                if (right - left + 1) > len(max_length_substring):
-                    max_length_substring = s[left:right+1]
-
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                if (right - left) + 1 > len(max_length):
+                    max_length = s[left:right+1]
                 left -= 1
-                right += 1
-            
+                right += 1  
+        
 
-            # odd
-            # odd_s = getPalindrome(i, i, s)
-
-            # # even
-            # even_s = getPalindrome(i, i + 1, s)
-
-            # if len(odd_s) > len(even_s) and len(odd_s) > max_length:
-            #     max_length = len(odd_s)
-            #     max_length_substring = odd_s
-
-            # elif len(even_s) > len(odd_s) and len(even_s) > max_length:
-            #     max_length = len(even_s)
-            #     max_length_substring = even_s
-
-        return max_length_substring
-
-
-
-
-
-
-
-
-
-        return max_length_substring
-
-            
-
-            
-
-            
-
-
-
-
-
+        return max_length
