2a0ce9a673872baab8f3df57395d970ab8c0cdbd
Anish Pokhrel <66049909+apokhrel7@users.noreply.github.com>
2025-05-09 11:53:02 -0600

    Time: 111 ms (20.99%) | Memory: 28.6 MB (24.20%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -1,28 +1,17 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        
-        # initialize left and right, record max container amount
-        # if left line < right line: move left line up
-        # else: move right line up 
-
-        left, right = 0, len(height) - 1
+        l, r = 0, len(height) - 1
 
         max_area = 0
+        while l < r:
+            curr_area = (r - l) * min(height[l], height[r])
 
-        # repeat until there are no more two lines that can hold water between them
-        while left < right:
+            max_area = max(max_area, curr_area) 
 
-            # area is the distance between leftmost and rightmost lines times the smallest height of among them 
-            area = (right - left) * min(height[left], height[right])
-            max_area = max(max_area, area)
-
-            # move left pointer up if left line is smaller than right line
-            if height[left] < height[right]:
-                left += 1
-
-            # otherwise move right pointer as right line is smaller than left
+            if height[l] <= height[r]:
+                l += 1
             else:
-                right -= 1
+                r -= 1
 
         return max_area
 
