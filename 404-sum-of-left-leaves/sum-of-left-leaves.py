0b63a943cdb04c129324ac7a1f849529fba6ba67
Anish Pokhrel <66049909+apokhrel7@users.noreply.github.com>
2025-05-12 10:58:47 -0600

    Time: 0 ms (100.00%) | Memory: 18.1 MB (47.54%) - LeetSync

--- 404-sum-of-left-leaves/sum-of-left-leaves.py
+++ 404-sum-of-left-leaves/sum-of-left-leaves.py
@@ -6,19 +6,20 @@
 #         self.right = right
 class Solution:
     def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
-        def dfs(node):
+        # check if left node exists and left of that left node is null and right of that left node is null
+        # if above is true, then is a left leaf
+        # recurse on left and right subtrees
 
-            # leaf node is always 0
+        self.count = 0
+        
+        def dfs(node):
             if not node:
                 return 0
 
-            # if node is a leaf node (found if it doesnt have any left or right children)
-            # then add the value of that node + the left leaf of its right side (recurse on the right subtree of the parent)
             if node.left and not node.left.left and not node.left.right:
-                return node.left.val + dfs(node.right)
-
-            # return the result of left and right subtree up to the parent
-            # this also covers the case where a leaf node is not found yet, so recurse on left and right subtrees
-            return dfs(node.left) + dfs(node.right)
+                self.count += node.left.val
             
-        return dfs(root)
+            return dfs(node.left) + dfs(node.right)
+
+        dfs(root)
+        return self.count
