fb7fd6d3ed8424293ba205986e26a7353ac637f5
Anish Pokhrel <66049909+apokhrel7@users.noreply.github.com>
2025-05-12 11:01:26 -0600

    Time: 0 ms (100.00%) | Memory: 18 MB (77.70%) - LeetSync

--- 404-sum-of-left-leaves/sum-of-left-leaves.py
+++ 404-sum-of-left-leaves/sum-of-left-leaves.py
@@ -9,17 +9,15 @@ class Solution:
         # check if left node exists and left of that left node is null and right of that left node is null
         # if above is true, then is a left leaf
         # recurse on left and right subtrees
-
-        self.count = 0
         
         def dfs(node):
             if not node:
                 return 0
 
             if node.left and not node.left.left and not node.left.right:
-                self.count += node.left.val
+                return node.left.val + dfs(node.right)  # explore right subtree of this node as it could have left leaves
             
             return dfs(node.left) + dfs(node.right)
 
-        dfs(root)
-        return self.count
+        return dfs(root)
+        
