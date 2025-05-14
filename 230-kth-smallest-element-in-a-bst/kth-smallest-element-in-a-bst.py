c4b64102e150d1cfec3b4dd0b9ef8c34bd6bdbc5
Anish Pokhrel <66049909+apokhrel7@users.noreply.github.com>
2025-05-14 14:03:58 -0600

    Time: 0 ms (100.00%) | Memory: 21.1 MB (72.39%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
@@ -6,31 +6,19 @@
 #         self.right = right
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        # traverse left first, then right
-        # count how many times to come up to parent
-
-        # just add nodes to list until it reaches k elements so return last one
-
-        ## OPTIMIZED Space-complexitty ##
-        # Runtime: O(h), Space: O(k) instead of previous O(n)
+        # traverse left tree first, then right
+        # track all nodes traversed
 
         res = []
+
         def dfs(node):
             if not node:
                 return
 
             dfs(node.left)
-
-            # stops recursion as soon as kth smallest element reached
-            if len(res) == k:
-                return
-            
             res.append(node.val)
             dfs(node.right)
 
-        
         dfs(root)
-        return res[-1]
-
-
         
+        return res[k-1]
