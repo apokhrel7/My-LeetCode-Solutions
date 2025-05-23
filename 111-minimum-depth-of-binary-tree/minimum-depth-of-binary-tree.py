219094c184872032aca2c9a583543888ac759e69
Anish Pokhrel <66049909+apokhrel7@users.noreply.github.com>
2025-05-23 16:22:15 -0600

    Time: 0 ms (100.00%) | Memory: 49.7 MB (86.18%) - LeetSync

--- 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.py
+++ 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -6,21 +8,49 @@
 #         self.right = right
 class Solution:
     def minDepth(self, root: Optional[TreeNode]) -> int:
-        if not root:
-            return 0
+        #### DFS ####
 
-        # if leaf node found, return the first depth
-        if not root.left and not root.right:
-            return 1
+        # if not root:
+        #     return 0
 
-        # if only left subtree has children, recurse on the left child
-        if root.left and not root.right:
-            return self.minDepth(root.left) + 1
+        # # if leaf node found, return the first depth
+        # if not root.left and not root.right:
+        #     return 1
 
-        # recurse on right subtree
-        if root.right and not root.left:
-            return self.minDepth(root.right) + 1
+        # # if only left subtree has children, recurse on the left child
+        # if root.left and not root.right:
+        #     return self.minDepth(root.left) + 1
 
-        # if both left and right children exist, recurse on both and return the minimum depth
-        return min(self.minDepth(root.left), self.minDepth(root.right)) + 1
+        # # recurse on right subtree
+        # if root.right and not root.left:
+        #     return self.minDepth(root.right) + 1
 
+        # # if both left and right children exist, recurse on both and return the minimum depth
+        # return min(self.minDepth(root.left), self.minDepth(root.right)) + 1
+
+
+        #### BFS ####
+        if not root: return 0
+
+        q = deque([root])
+        min_depth = 1  # since we have a root, start with depth as 1
+        
+        while q:
+
+            # take snapshot of length of queue since we are doing BFS (level by level)
+            q_length = len(q)
+            for _ in range(q_length):
+                node = q.popleft()
+
+                if not node.left and not node.right:
+                    return min_depth
+                
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+
+            # at this point, we have gone through a level in the tree
+            min_depth += 1
+        
+        return min_depth
