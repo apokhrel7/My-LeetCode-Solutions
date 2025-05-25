cda03de3513d7ce70000124ca2f996ae31e28bbc
Anish Pokhrel <66049909+apokhrel7@users.noreply.github.com>
2025-05-25 00:00:19 -0600

    Time: 3 ms (37.03%) | Memory: 21.2 MB (27.09%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
@@ -6,19 +6,33 @@
 #         self.right = right
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        # traverse left tree first, then right
-        # track all nodes traversed
-
-        res = []
-
+        """
+        - traverse to leftmost leaf node (gives you 1st smallest value)
+        - count up from there going right
+        - then going up to parent
+        """
+        self.count = 0
+        self.theVal = 0
         def dfs(node):
+            # if we reach leaf node, start counting from there so return back up
             if not node:
                 return
 
             dfs(node.left)
-            res.append(node.val)
+            
+            # left leaf node has been reached, start counting
+            self.count += 1
+
+            # return early if kth smallest is found using the count
+            if k == self.count:
+                self.theVal = node.val
+                return
+
+            # otherwise traverse right subtree
             dfs(node.right)
 
+            
         dfs(root)
-        
-        return res[k-1]
+        return self.theVal
+
+
