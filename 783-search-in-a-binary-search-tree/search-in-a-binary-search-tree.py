d264d9ba207b384099dd4b6754bdf0defc15f549
Anish Pokhrel <66049909+apokhrel7@users.noreply.github.com>
2025-05-24 23:31:24 -0600

    Time: 0 ms (100.00%) | Memory: 19.3 MB (44.21%) - LeetSync

--- 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.py
+++ 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -6,16 +8,35 @@
 #         self.right = right
 class Solution:
     def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
-        def dfs(node):
+        # def dfs(node):
+        #     if not node:
+        #         return None
+
+        #     if node.val == val:
+        #         return node
+
+        #     if node.val < val:
+        #         return dfs(node.right)
+
+        #     return dfs(node.left)
+
+        # return dfs(root)
+
+        q = deque([root])
+
+        while q:
+            node = q.popleft()
+
             if not node:
                 return None
-                
-            if node.val == val:
+
+            elif node.val == val:
                 return node
 
-            if node.val < val:
-                return dfs(node.right)
+            elif node.val < val:
+                q.append(node.right)
+            
+            else:
+                q.append(node.left)
 
-            return dfs(node.left)
-
-        return dfs(root)
+        return None
