2bd54aef56fddceb0ac42de1de330ebb0db57268
Anish Pokhrel <66049909+apokhrel7@users.noreply.github.com>
2025-05-26 11:36:19 -0600

    Time: 18 ms (39.33%) | Memory: 20.6 MB (89.66%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,29 +1,27 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # sort each character of each word,
+        """
+        1. sort each word and add it to a hash map. this way we can group anagrams later
+        2. go through list of strings, sort the string, add it to the corresponding list according to the sorted words hashset
+        """
 
-        # use hashmap to count occurances of letters in order to group the letters
+        hash_map = {}
 
-    
+        for word in strs:
+            sorted_word = ''.join(sorted(word))
+            hash_map[sorted_word] = []
 
-        dict = {}
+        # {ate: [], bat: [], ant: []}
         res = []
 
-        
-
         for word in strs:
-            dict[''.join(sorted(word))] = []
+            sorted_word = ''.join(sorted(word))
+            hash_map[sorted_word].append(word)
 
-        for word in strs:
-            dict[''.join(sorted(word))].append(word)
-
-        for word in dict.values():
-            res.append(word)
+        for list_words in hash_map.values():
+            res.append(list_words)
 
         return res
-
-
-        
 
 
         
