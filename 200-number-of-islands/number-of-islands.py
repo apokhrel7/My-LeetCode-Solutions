b9fd168c71edafb420238a8cc59bef5fcad36576
Anish Pokhrel <66049909+apokhrel7@users.noreply.github.com>
2025-05-26 23:20:14 -0600

    Time: 261 ms (40.77%) | Memory: 25.6 MB (17.79%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -2,40 +2,39 @@ from collections import deque
 
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        ROWS, COLS = len(grid), len(grid[0])
-        visited = set()  # track which islands have been visited
+        num_islands = 0
 
+        ROWS, COLS = len(grid), len(grid[0])
+        seen = set()
         q = deque()
 
-        # find out which grids contain an island, then span out to "see" how big it is
-        # this way count that island as 1 island
-        # look for others
         def bfs(r, c):
-            DIRECTIONS = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+            DIRECTIONS = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 
             q.append((r, c))
 
             while q:
-                row, col = q.popleft()
-                for x, y in DIRECTIONS:
-                    new_row, new_col = row + x, col + y
+                i, j = q.popleft()
 
-                    if 0 <= new_row < ROWS and 0 <= new_col < COLS and (new_row, new_col) not in visited and grid[new_row][new_col] == "1": 
-                        visited.add((new_row, new_col))
-                        q.append((new_row, new_col)) 
-                        
-        num_islands = 0
-        for row in range(ROWS):
-            for col in range(COLS):
-                if grid[row][col] == "1" and (row, col) not in visited:
-                    visited.add((row, col))
-                    bfs(row, col)
-                    num_islands += 1 
+                for x, y in DIRECTIONS:
+                    new_i, new_j = i + x, j + y
+                    
+                    if 0 <= new_i < ROWS and 0 <= new_j < COLS and (new_i, new_j) not in seen and grid[new_i][new_j] == '1':
+                        seen.add((new_i, new_j))
+                        q.append((new_i, new_j))
 
         
-
+        # loop through entire grid and expand out when island is found
+        for row in range(ROWS):
+            for col in range(COLS):
+                if (row, col) not in seen and grid[row][col] == '1':
+                    # expand on this (row, col) using BFS
+                    # track the (row, col) that you have expanded on
+                    num_islands += 1
+                    seen.add((row, col))
+                    bfs(row, col)
 
         return num_islands
-
+        
 
         
