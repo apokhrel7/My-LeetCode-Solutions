147c1025f6b068fa0e459b28aa6b5f33a9fae1da
Anish Pokhrel <66049909+apokhrel7@users.noreply.github.com>
2025-05-26 18:30:49 -0600

    Time: 0 ms (100.00%) | Memory: 18 MB (50.54%) - LeetSync

---
+++ 733-flood-fill/flood-fill.py
@@ -0,0 +1,31 @@
+from collections import deque
+
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        # BFS
+        DIRECTIONS = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+        ROWS, COLS = len(image), len(image[0])
+        q = deque([(sr, sc)])
+
+        # save the target color
+        original_target = image[sr][sc]
+        image[sr][sc] = color 
+
+        seen = set()
+        
+
+        while q:
+            i, j = q.popleft()
+            for x, y in DIRECTIONS:
+                new_i, new_j = i + x, j + y
+
+                if (new_i, new_j) not in seen and 0 <= new_i < ROWS and 0 <= new_j < COLS and image[new_i][new_j] == original_target:
+                    image[new_i][new_j] = color
+                    q.append((new_i, new_j))
+                    seen.add((new_i, new_j))
+
+        return image
+
+
+
+            
