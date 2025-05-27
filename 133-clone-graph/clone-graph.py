1e7747bca30980572722ca2be5419588a4612205
Anish Pokhrel <66049909+apokhrel7@users.noreply.github.com>
2025-05-27 15:06:33 -0600

    Time: 50 ms (14.17%) | Memory: 18.1 MB (82.11%) - LeetSync

--- 133-clone-graph/clone-graph.py
+++ 133-clone-graph/clone-graph.py
@@ -11,47 +11,49 @@ class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
 
         ## DFS ##
-        #  oldToNew = {}
-        # def dfs(root):
-        #     if root in oldToNew:
-        #         return oldToNew[root]
+        oldToNew = {}
+        def dfs(root):
+            if root in oldToNew:
+                return oldToNew[root]
 
-        #     copy = Node(root.val)
-        #     oldToNew[root] = copy
+            # copy = Node(root.val)
+            # oldToNew[root] = copy
 
-        #     # visit this node's neighbours
-        #     for nei in root.neighbors:
-        #         copy.neighbors.append(dfs(nei))
+            oldToNew[root] = Node(root.val)
 
-        #     # return the copy back to parent to make all the connections in both directions (undirected)
-        #     return copy
+            # visit this node's neighbours
+            for nei in root.neighbors:
+                oldToNew[root].neighbors.append(dfs(nei))
+
+            # return the copy back to parent to make all the connections in both directions (undirected)
+            return oldToNew[root]
         
-        # return dfs(node) if node else None
+        return dfs(node) if node else None
 
         ## BFS ##
         # Base case
-        if not node: return None
+        # if not node: return None
         
-        oldToNew = {}
-        oldToNew[node] = Node(node.val)
-        q = deque([node])
+        # oldToNew = {}
+        # oldToNew[node] = Node(node.val)
+        # q = deque([node])
 
-        while q:
-            curr_node = q.popleft()
+        # while q:
+        #     curr_node = q.popleft()
 
-            # go through neibours of a node
-            for nei in curr_node.neighbors:
-                # if neighbour is not stored in hash map, then store a copy of it then add the neighbour to next in queue
-                if nei not in oldToNew:
-                    q.append(nei)
-                    oldToNew[nei] = Node(nei.val)
+        #     # go through neibours of a node
+        #     for nei in curr_node.neighbors:
+        #         # if neighbour is not stored in hash map, then store a copy of it then add the neighbour to next in queue
+        #         if nei not in oldToNew:
+        #             q.append(nei)
+        #             oldToNew[nei] = Node(nei.val)
 
-                # reference the copy of the current node, and add the neighbour copy to the adjacency list
-                # here we are essentially doing copied_node.neighbors.append(copied_neighbor)
-                oldToNew[curr_node].neighbors.append(oldToNew[nei])
+        #         # reference the copy of the current node, and add the neighbour copy to the adjacency list
+        #         # here we are essentially doing copied_node.neighbors.append(copied_neighbor)
+        #         oldToNew[curr_node].neighbors.append(oldToNew[nei])
 
-        # return head of original node
-        return oldToNew[node]
+        # # return head of original node
+        # return oldToNew[node]
                     
 
                 
