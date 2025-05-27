024592674581d7259620143ef74f2bc60dad8c64
Anish Pokhrel <66049909+apokhrel7@users.noreply.github.com>
2025-05-27 16:24:35 -0600

    Time: 7 ms (47.55%) | Memory: 20 MB (8.14%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -2,41 +2,47 @@ class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         adj_list = {}
 
-        # initialize adjacency list
+        # adj_list = {0: [], 1: [0]}
+
+        # create adjacency list
         for i in range(numCourses):
             adj_list[i] = []
 
-        # populate adjacency list
+        # populate adjacency_list
         for crs, pre in prerequisites:
             adj_list[crs].append(pre)
 
-        potential_cycles = set()  # keep track of potential courses that could be cycles
-        seen = set()              # keep track of courses we have already checked and verified to not be cycles
+        potential_cycle = set()  # track courses that could be potential cycles
+        visited = set()          # track courses that you have already seen, this increases efficiency as we don't have to check courses we have already visited for cycles
 
+        def dfs(crs):
 
-        def dfs(course):
-            if course in potential_cycles:
+            # Base case 1: if cyclical refernce to same course detected (cycle detected), return False
+            if crs in potential_cycle:
                 return False
 
-            if course in seen:
+            # if we are visiting a course again but there is no cycle, return True
+            if crs in visited:
                 return True
 
-            potential_cycles.add(course)        # this course could be a cycle, so add it to list and check
+            # new unseen course, so add it to a set of potential cycles
+            potential_cycle.add(crs)
 
-            # check cycles by going through all the prerequisites of a course
-            for pre in adj_list[course]:
-                if not dfs(pre):
+            # go through the neigbours of this node and recurse on them to detect cycles
+            for pre in adj_list[crs]:
+                if not dfs(pre): 
                     return False
 
-            potential_cycles.remove(course)     # at this point we have verified that this course doesn't have cycles (at the moment)
-            seen.add(course)                    # we have seen this course, so remember it (reduced time complexity?)
+            # at this point, no cycles detected for this course so remove it from list of potential cycles
+            potential_cycle.remove(crs)
+            
+            # remember this course as we have checked it for cycles
+            visited.add(crs)
+            return True  # at this point, everything is looking good, return True
 
-            return True
-
-
-        # need to check every course
-        for course in range(numCourses):
-            if not dfs(course):
+        # we have to run this cycle detection for every course
+        for i in range(numCourses):
+            if not dfs(i):
                 return False
 
         return True
