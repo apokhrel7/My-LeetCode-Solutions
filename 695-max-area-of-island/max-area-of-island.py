0140d099ad51fa1f009ee9ff1b3c5415b36c9464
Anish Pokhrel <66049909+apokhrel7@users.noreply.github.com>
2025-05-27 15:32:27 -0600

    Time: 19 ms (73.34%) | Memory: 17.8 MB (99.62%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.py
+++ 695-max-area-of-island/max-area-of-island.py
@@ -18,22 +18,33 @@ class Solution:
                 for x, y in DIRECTIONS:
                     new_i, new_j = i + x, j + y
 
+                    # if (
+                    #     0 <= new_i < ROWS
+                    #     and 0 <= new_j < COLS
+                    #     and (new_i, new_j) not in visited
+                    #     and grid[new_i][new_j] == 1
+                    # ):
+                    #     curr_area += 1
+                    #     q.append((new_i, new_j))
+                    #     visited.add((new_i, new_j))
                     if (
                         0 <= new_i < ROWS
                         and 0 <= new_j < COLS
-                        and (new_i, new_j) not in visited
                         and grid[new_i][new_j] == 1
                     ):
                         curr_area += 1
                         q.append((new_i, new_j))
-                        visited.add((new_i, new_j))
+                        grid[new_i][new_j] = -1
 
             return curr_area
 
         for row in range(ROWS):
             for col in range(COLS):
-                if (row, col) not in visited and grid[row][col] == 1:
-                    visited.add((row, col))
+                # if (row, col) not in visited and grid[row][col] == 1:
+                #     visited.add((row, col))
+                #     max_area = max(max_area, bfs(row, col))
+                if grid[row][col] == 1:
+                    grid[row][col] = -1
                     max_area = max(max_area, bfs(row, col))
 
         return max_area
