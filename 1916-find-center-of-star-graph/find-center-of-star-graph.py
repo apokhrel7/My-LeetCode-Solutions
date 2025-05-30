af5ff39f99d9d20bbd4f7bfac5657edd397c5320
Anish Pokhrel <66049909+apokhrel7@users.noreply.github.com>
2025-05-29 18:21:39 -0600

    Time: 0 ms (100.00%) | Memory: 45.5 MB (78.74%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.py
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.py
@@ -2,17 +2,24 @@ class Solution:
     def findCenter(self, edges: List[List[int]]) -> int:
         # centre graph must have out degree of n-1
 
-        degrees = [0] * (10**5 + 1)
+        # degrees = [0] * (10**5 + 1)
 
-        max_edge = 0
-        star_index = 0
-        for u, v in edges:
-            degrees[u] += 1
-            degrees[v] += 1
+        # max_edge = 0
+        # star_index = 0
+        # for u, v in edges:
+        #     degrees[u] += 1
+        #     degrees[v] += 1
 
-        for i in range(1, len(degrees)):
-            if degrees[i] > max_edge:
-                star_index = i
-                max_edge = degrees[i]
+        # for i in range(1, len(degrees)):
+        #     if degrees[i] > max_edge:
+        #         star_index = i
+        #         max_edge = degrees[i]
 
-        return star_index
+        # return star_index
+
+        if edges[0][1] == edges[1][0] or edges[0][1] == edges[1][1]:
+            return edges[0][1]
+        if edges[0][0] == edges[1][1] or edges[0][0] == edges[1][0]:
+            return edges[0][0]
+        
+        
