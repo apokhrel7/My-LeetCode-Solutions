c838c55f1ce907124b8d7748063176a11dd13d35
Anish Pokhrel <66049909+apokhrel7@users.noreply.github.com>
2025-09-11 21:32:19 -0600

    Time: 9 ms (15.32%) | Memory: 18.8 MB (9.78%) - LeetSync

--- 684-redundant-connection/redundant-connection.py
+++ 684-redundant-connection/redundant-connection.py
@@ -1,38 +1,29 @@
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        # Cycle detection on undirected graph
-
         adj_list = {}
-
-        # build adjacency list
-        for i in range(1, len(edges) + 1):
+        n = len(edges)
+        for i in range(1, n + 1):
             adj_list[i] = []
 
-        # populate adjacency list
-        # for a, b in edges:
-        #     adj_list[a].append(b)
-        #     adj_list[b].append(a)
 
-
-        def find_cycle(node, prev, visited):
+        def dfs_is_cycle_detected(node, prev, visited):
             if node in visited:
                 return True
 
             visited.add(node)
-            for b in adj_list[node]:
-                if b == prev:
+            for nei in adj_list[node]:
+                if nei == prev:
                     continue
-                if find_cycle(b, node, visited):
+
+                if dfs_is_cycle_detected(nei, node, visited):
                     return True
-                    
+
             return False
+
 
         for a, b in edges:
             adj_list[a].append(b)
             adj_list[b].append(a)
-            
-            if find_cycle(a, -1, set()):
-                return [a, b]
-        
-        return []
 
+            if dfs_is_cycle_detected(a, b, set()):
+                return [a, b]
