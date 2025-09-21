472a12bec20bcec11aa3ca219b66933168532896
Anish Pokhrel <66049909+apokhrel7@users.noreply.github.com>
2025-09-21 01:00:05 -0600

    Time: 0 ms (100.00%) | Memory: 17.7 MB (69.38%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,8 +1,5 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-
-        # create hashmap/dictionary of digits to their corresponding letters
-
         digits_to_letters = {
             "2": "abc",
             "3": "def",
@@ -11,26 +8,22 @@ class Solution:
             "6": "mno",
             "7": "pqrs",
             "8": "tuv",
-            "9": "wxyz"
+            "9": "wxyz",
         }
 
         res = []
+
         n = len(digits)
-        def backtracking(i, curr_string):
-            # once all the digits have been traversed, add the combination to resulting list and return
-            if i >= n:
-                res.append(curr_string)
+
+        def backtracking(i, curr_str):
+            if len(curr_str) == n:
+                res.append(curr_str)
                 return
+            
+            for char in digits_to_letters[digits[i]]:
+                backtracking(i + 1, curr_str + char)
 
-            # go through each mappiong of digits to letters, going to each ith digit as well
-            for digit in digits_to_letters[digits[i]]:
-                backtracking(i + 1, curr_string + digit)   # combine i with i + 1, also combining the strings with each letter
-
-        # if digits is non-empty, then run backtracking algorithm
-        if digits:
-            backtracking(0, "")
+        if not digits:
+            return []
+        backtracking(0, "")
         return res
-
-
-        
-        
