88e141c39d8d93a8ef585d6c604d6d98c79bd0f4
Anish Pokhrel <66049909+apokhrel7@users.noreply.github.com>
2025-10-15 23:34:22 -0600

    Time: 3 ms (76.17%) | Memory: 19.9 MB (11.81%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.py
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -1,35 +1,37 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        adj_list = {}
         res = []
-        potential_cycle = set()
-        visited = set()
 
-        # initialize adjacency list
+        adj_list = {}
         for i in range(numCourses):
             adj_list[i] = []
 
-        # populate adjacency list
-        for pre, course in prerequisites:
-            adj_list[pre].append(course)
+        for crs, pre in prerequisites:
+            adj_list[crs].append(pre)
 
-        def dfs(crs):
-            if crs in visited:
-                return True
+        potential_cycles = set()
+        visited = set()
 
-            if crs in potential_cycle:
+        def dfs(node):
+            if node in potential_cycles:
                 return False
 
-            potential_cycle.add(crs)
-            for pre in adj_list[crs]:
-                if not dfs(pre): return False
-            
-            potential_cycle.remove(crs)
-            visited.add(crs)
-            res.append(crs)
+            if node in visited:
+                return True
+
+            potential_cycles.add(node)            
+            for pre in adj_list[node]:
+                if not dfs(pre):
+                    return False
+                    
+            visited.add(node)
+            potential_cycles.remove(node)
+            res.append(node)
+
             return True
 
         for i in range(numCourses):
-            if not dfs(i): return []
-            
+            if not dfs(i):
+                return []
+
         return res
